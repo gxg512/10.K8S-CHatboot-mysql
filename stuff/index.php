7bced66c642feb39bba5f03cb7ff4e451e16facc
Your Name <you@example.com>
2022-11-11 16:13:30 +0200

    updated scripts with secrets

--- stuff/index.php
+++ stuff/index.php
@@ -1,9 +1,9 @@
 <?php
 
-$host="172.17.02.4";
-$user="db_usera";
-$pass="password";
-$db="chat";
+$host="mysql.default.svc.cluster.local";
+$user=$_ENV['MYSQL_USER'];
+$pass=$_ENV['MYSQL_PASSWORD'];
+$db=$_ENV['MYSQL_DATABASE'];
 
 $con=mysqli_connect($host,$user,$pass,$db);
 
